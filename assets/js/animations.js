fa405515a486a8bccbe9901621648e7bf8d5c1ae
jakesw87 <jakesw87@gmail.com>
2020-12-27 13:47:13 -0600

    added looping keyframes and listings for skills and percentages. almost complete

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,28 +1,19 @@
 
 // this stops a keyframe being used in author_skills.html include. the keyframe should be integrated with main.scss when possible
 function removeGrow(elem) {
-    console.log('that')
     elem.classList.remove('grow');
 };
 
 function addGrow(elem) {
-    console.log('this')
     elem.classList.add('grow');
 };
 
-function showGrow(entries, observer) {
+function showGrow(entries) {
     entries.forEach(entry => {
-        console.log(entry);
         addGrow(entry.target);
-        setTimeout(function() {removeGrow(entry.target)}, 1800);
+        setTimeout(function() {removeGrow(entry.target)}, 1200);
     });
-    console.log(observer);
 };
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     showGrow(document.querySelector('.watch-grow'));
-// });
-
 
 let skillbar_observer = {
     rootMargin: '0px',
