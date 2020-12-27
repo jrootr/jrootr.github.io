f31c168628a1e227eed8d555d9da5c3d85be6ac3
jakesw87 <jakesw87@gmail.com>
2020-12-27 11:53:45 -0600

    intersection listener working, just need to refine functions

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,8 +1,36 @@
 
-// this stops a keyframe being used in author_skills.html include. the keyframe should be integrated with main.scss
-function removeGrow() {
-    document.querySelector('.grow').classList.remove('grow');
+// this stops a keyframe being used in author_skills.html include. the keyframe should be integrated with main.scss when possible
+function removeGrow(elem) {
+    console.log('that')
+    elem.classList.remove('grow');
+};
+
+function addGrow(elem) {
+    console.log('this')
+    elem.classList.add('grow');
+};
+
+function showGrow(entries, observer) {
+    entries.forEach(entry => {
+        console.log(entry);
+        addGrow(entry.target);
+        setTimeout(function() {removeGrow(entry.target)}, 1800);
+    });
+    console.log(observer);
+};
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     showGrow(document.querySelector('.watch-grow'));
+// });
+
+
+let skillbar_observer = {
+    rootMargin: '0px',
+    threshold: 1.0
 }
+
 document.addEventListener('DOMContentLoaded', () => {
-    setTimeout(removeGrow, 1800);
+    let observer = new IntersectionObserver(showGrow, skillbar_observer);
+    let target = document.querySelector('.watch-grow');
+    observer.observe(target);
 });
